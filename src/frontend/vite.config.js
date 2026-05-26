d54b39fc4cb209cf67a3e25657f7fa90c3f32794
minhvy259 <vyho0509@gmail.com>
2026-05-26 14:14:29 +0700

    feat/DE190408-Tao kich ban github actons

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'swp391-su26-ai-audit-project-swp391_se20a02_group-03',
 })
